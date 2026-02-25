37cc90ab0bf68fbd46d7c2597dcafe9a8b2bf503
chazs0409 <chazs0409@gmail.com>
2026-02-25 13:20:07 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import HealthCheck from './components/HealthCheck';
 function App() {
   return (
     <div className="App">
-      <h1>Job Tracker Application</h1>
+      <h1>Job Tracker Application Chastity</h1>
       <HealthCheck />
     </div>
   );
