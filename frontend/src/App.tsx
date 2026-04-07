5de590e39ee2c100081f9aed3cd96974e8b94640
JYLDYZ <jtorogulova3883@gmail.com>
2026-04-07 15:38:30 -0500

    Feature: Create Admin Dashboard with functions : Adding company and job (with dumb buttons)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import SearchPage from "./components/SearchPage";
+import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
